42340df0984b73b7a0abb5bed2c186279e10a0ef
myleftshoe <paulesammut@gmail.com>
2022-05-23 08:49:00 +1000

    .

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,18 @@
 <script context="module">
-    import List from '$lib/components/List.svelte'
-    import ListItem from '$lib/components/ListItem.svelte'
+    const items = Array.from({length: 5}, (v, i) => ({
+        primary: `primary ${i}`,
+        secondary: `secondary ${i}`,
+    }))
 </script>
-<List>
-    <ListItem>
-        <div>primary 1</div>
-        <div class="text-info">secondary1</div>
-        <div slot="meta">meta</div>
-    </ListItem>
-    <ListItem>
-        <div>item 2</div>
-        <div slot="meta">meta2</div>
-    </ListItem>
-    <ListItem>
-        <div>item 3</div>
-        <div slot="meta">meta3</div>
-    </ListItem>
-</List>
+<ul class="flex flex-col divide-y divide-zinc-500">
+    {#each items as { primary, secondary}}
+    <li class="flex flex-row justify-between py-4 px-6 text-base-content">
+        <div>
+            <div>primary 1</div>
+            <div>secondary1</div>
+        </div>
+        <div>meta</div>
+    </li>
+    {/each}
+</ul>
 
