a1ed411e8d8961f6e773518fdcc2cb9bbfe64879
myleftshoe <paulesammut@gmail.com>
2022-05-23 14:06:33 +1000

    good

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script context="module">
+    import Ripple from '$lib/actions/Ripple'
     const items = Array.from({length: 5}, (v, i) => ({
         primary: `primary ${i}`,
         secondary: `secondary ${i}`,
@@ -6,7 +7,7 @@
 </script>
 <ul class="flex flex-col divide-y divide-zinc-500">
     {#each items as { primary, secondary}}
-    <li class="flex flex-row justify-between py-4 px-6 text-base-content">
+    <li class="flex flex-row justify-between py-4 px-6 text-base-content" use:Ripple={{ centered: false, color: 'blue' }}>
         <div>
             <div>primary 1</div>
             <div>secondary1</div>
@@ -16,3 +17,6 @@
     {/each}
 </ul>
 
+<button class="btn" use:Ripple={{ centered: false }}>
+    test
+</button>
