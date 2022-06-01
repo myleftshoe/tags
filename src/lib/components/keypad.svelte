29f49887aeabe59568521def6c87807a9de0a251
myleftshoe <paulesammut@gmail.com>
2022-06-01 19:21:02 +1000

    button hover accent color same as normal state

--- src/lib/components/keypad.svelte
+++ src/lib/components/keypad.svelte
@@ -29,18 +29,18 @@
     <h1 class="text-3xl text-base-content">Enter PIN</h1>
     <h1 class="text-3xl tracking-widest text-accent-focus">{view}</h1>
     <keypad>
-        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal" on:click={select(1)}>1</key>
-        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal" on:click={select(2)}>2</key>
-        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal" on:click={select(3)}>3</key>
-        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal" on:click={select(4)}>4</key>
-        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal" on:click={select(5)}>5</key>
-        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal" on:click={select(6)}>6</key>
-        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal" on:click={select(7)}>7</key>
-        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal" on:click={select(8)}>8</key>
-        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal" on:click={select(9)}>9</key>
+        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" on:click={select(1)}>1</key>
+        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" on:click={select(2)}>2</key>
+        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" on:click={select(3)}>3</key>
+        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" on:click={select(4)}>4</key>
+        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" on:click={select(5)}>5</key>
+        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" on:click={select(6)}>6</key>
+        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" on:click={select(7)}>7</key>
+        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" on:click={select(8)}>8</key>
+        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" on:click={select(9)}>9</key>
         <p />
         <!-- <button class="btn bg-primary" disabled={!value} on:click={clear}>clear</button> -->
-        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-xl font-normal" on:click={select(0)}>0</key>
+        <key class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" on:click={select(0)}>0</key>
         <!-- <button disabled={!value} on:click={submit}>submit</button> -->
     </keypad>
     <button class="btn btn-link text-accent {!value && 'invisible'}" on:click={() => (value = '')}>delete</button>
