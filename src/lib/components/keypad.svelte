bfebdae9c222edac88c736ae9ae95e8b15301b57
myleftshoe <paulesammut@gmail.com>
2022-06-03 22:07:57 +1000

    keypad tweak

--- src/lib/components/keypad.svelte
+++ src/lib/components/keypad.svelte
@@ -10,7 +10,6 @@
     let value = ''
 
     const select = (num) => async () => {
-        console.log({ num })
         value += num
         if (value === '2086') {
             console.log('Correct pin entered')
@@ -30,11 +29,14 @@
     <h1 class="text-3xl text-base-content">Enter PIN</h1>
     <h1 class="text-3xl tracking-widest text-accent-focus">{view}</h1>
     <keypad>
-        {#each [1,2,3,4,5,6,7,8,9] as key}
-            <button disabled={!browser} class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" on:click={select(key)}>{key}</button>
+        {#each [1,2,3,4,5,6,7,8,9,null,0] as key}
+            <button on:click={select(key)}
+                disabled={!browser} 
+                class="{key === null ? 'invisible' : ''} btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" 
+            >
+                {key}
+            </button>
         {/each}
-        <p />
-        <button disabled={!browser} class="btn btn-circle btn-lg text-accent-content bg-accent text-2xl font-normal hover:bg-accent active:bg-accent-focus" on:click={select(0)}>0</button>
     </keypad>
     <button class="btn btn-link text-accent {!value && 'invisible'}" on:click={() => (value = '')}>delete</button>
 </main>
@@ -46,12 +48,8 @@
         place-content: space-around;
         padding: 2em 0;
         gap: 2em;
-        /* height: 100vmin; */
-        /* width: 100vw; */
     }
     keypad {
-        /* padding:1em; */
-        /* background-color: green; */
         display: grid;
         place-content: end;
         place-items: center;
