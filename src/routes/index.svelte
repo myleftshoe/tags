7b8685d72825ae3e5e9d381b8f5c0ff000715219
myleftshoe <paulesammut@gmail.com>
2022-06-03 12:08:26 +1000

    no sub and sup on price

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -61,17 +61,10 @@
 <ul class="flex flex-col divide-y divide-base-300">
     {#each items as item}
         <li class="flex flex-row gap-3 justify-between py-4 px-4 text-base-content bg-base-100 active:bg-base-200" on:click={handleItemClick(item)}>
-            <div class="flex flex-row justify-center items-center w-1/3" style={`${item.status === 'unbound' && 'opacity:.50'}`} >
-                <label for="price-modal">
-                    <sup>$</sup>
-                    <span class="text-xl">{dollars(item.price)}</span>
-                    <sup>{cents(item.price)}</sup>
-                    <sub >{item.label10}</sub>
-                    <!-- <span class="text-xs relative -right-1 -bottom-2">{item.label10}</span> -->
-                    <!-- <div>{item.label10}</div> -->
-                </label>
+            <div class="w-1/4 text-right pr-10" style={`${item.status === 'unbound' && 'opacity:.50'}`} >
+                <price class="text-xl" data-cents="{cents(item.price)}" data-unit="{item.label10}">{dollars(item.price)}</price>
             </div>
-            <div class="w-2/3 flex flex-col justify-center" style={`${item.status === 'unbound' && 'opacity:.50'}`}>
+            <div class="w-3/4 flex flex-col justify-center" style={`${item.status === 'unbound' && 'opacity:.50'}`}>
                 <label for="edit-modal">
                     {`${item.label4.trim()} ${item.label5.trim()}`.trim()}
                     <!-- <div>{item.label5}</div>
@@ -110,3 +103,19 @@
     </div>
 </Overlay> -->
 
+<style>
+    price:before { 
+        font-size: .75rem;
+        content: "$"
+    }
+    price:after { 
+        position: absolute;
+        font-size: .75rem;
+        white-space: pre-line;
+        line-height: 2.5ch;
+        text-align: center;
+        content: attr(data-cents)"\a"attr(data-unit);
+        transform: translateX(.25ch);
+    }
+
+</style>
