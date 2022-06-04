4b7b15e45d439d61a28b6c8572c98f12799b9b77
myleftshoe <paulesammut@gmail.com>
2022-06-04 14:49:49 +1000

    fuzzy2 when search string is <= 2 return items that startWith

--- src/lib/util/fuzzy.js
+++ src/lib/util/fuzzy.js
@@ -26,7 +26,34 @@ function fuzzy1(array, value, fields) {
     return fuzzed
 }
 
+function fuzzy2(array, value, fields) {
+    if (value.length === 0) return [...array]
+
+    if (value.length <= 2) {
+        const matchedFirstChar = array.filter(item => {
+            for (const field of fields) {
+                if(item[field].startsWith(value))
+                    return true
+            }
+            return false
+        })
+        return matchedFirstChar
+    }
+
+    const picked = Array.isArray(fields) && array.map(pick(...fields)) || [...array]
+    const fuzzed = picked.reduce((acc, cur, index) => {
+        const stringified = Object.values(cur)
+            .join(' ')
+            .toUpperCase()
+            // .replace(nonAlpha, '')
+            .replace(spaces, ' ');
+        if (stringified.includes(value))
+            acc.push(array[index])
+        return acc
+    }, [])
+    return fuzzed
+}
 
 const pick = (...props) => o => props.reduce((a, e) => ({ ...a, [e]: o[e] }), {});
 
-export default fuzzy1
+export default fuzzy2
