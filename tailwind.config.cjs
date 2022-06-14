96d58a99fa2b06c8e12caa97a3c9e7e3f111aa2c
myleftshoe <paulesammut@gmail.com>
2022-06-14 17:19:40 +1000

    upgraded tailwind to 3.1.2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config'} */
 module.exports = {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     plugins: [require("@tailwindcss/typography"), require('daisyui')],
