cfd45f2e3ca4076321aabf6aa290dd5ea5780b7b
myleftshoe <paulesammut@gmail.com>
2022-06-16 19:32:28 +1000

    .

--- src/lib/components/keypad.svelte
+++ src/lib/components/keypad.svelte
@@ -20,37 +20,36 @@
             return
         }
     }
+
     $: view = value ? view.substring(0, value.length - 1) + '●' + view.substring(value.length) : '○○○○'
     $: console.log({browser})
 </script>
 
-<main class="glass bg-primary fixed inset-0 z-50">
-    <p class="text-2xl text-base-content select-none after:content-['*'] after:text-sm after:absolute">Enter PIN</p>
-    <h1 class="text-3xl tracking-widest text-accent-focus select-none">{view}</h1>
-    <keypad>
-        {#each [1,2,3,4,5,6,7,8,9,null,0] as key}
-            <button on:click={select(key)}
-                disabled={!browser} 
-                class="{key === null ? 'invisible' : ''} no-animation btn btn-circle border-primary-content opacity-50 btn-lg text-primary-content bg-transparent text-2xl font-normal hover:bg-accent active:bg-accent-focus" 
-            >
-                {key}
-            </button>
-        {/each}
-    </keypad>
-    <button class="btn btn-link text-accent {!value && 'invisible'}" on:click|stopPropagation={() => (value = '')}>delete</button>
-    <footer class="grid place-content-center w-full absolute bottom-4 text-sm text-base-content opacity-50 select-none">*or scan a tag to start</footer>
-</main>
+<div class="bg-primary-focus fixed inset-0 z-50">
+    <main class="glass h-full py-8">
+        <p class="text-2xl text-primary-content opacity-60 select-none after:content-['*'] after:text-sm after:absolute">Enter PIN</p>
+        <h1 class="text-3xl tracking-widest text-accent select-none">{view}</h1>
+        <keypad>
+            {#each [1,2,3,4,5,6,7,8,9,null,0] as key}
+                <button name="key" on:click={select(key)}
+                    disabled={!browser} 
+                    class="{key === null ? 'invisible' : ''} no-animation transition-none btn btn-circle border-primary-content opacity-60 btn-lg text-primary-content bg-accent/0 text-2xl font-normal hover:bg-transparent active:bg-accent active:border-accent hover:border-primary-content" 
+                >
+                    {key}
+                </button>
+            {/each}
+        </keypad>
+        <button class="btn btn-link text-accent {!value && 'invisible'}" on:click|stopPropagation={() => (value = '')}>delete</button>
+        <footer class="grid place-content-center w-full absolute bottom-4 text-sm text-primary-content opacity-50 select-none">*or scan a tag to start</footer>
+    </main>
+</div>
 
 <style>
     main { 
-        /* height: 95%; */
         align-self: flex-end;
         overflow: hidden;
         display: grid;
         place-items: center;
-        place-content: space-around;
-        padding: 2em 0;
-        gap: 2em;
     }
     keypad {
         display: grid;
@@ -59,14 +58,5 @@
         grid-template-columns: repeat(3, 5em);
         grid-template-rows: repeat(4, 4.5em);
         grid-gap: 0.5em;
-    }
-    /* .key {
-        @apply btn btn-circle btn-lg text-accent-content bg-accent text-xl font-normal;
-    } */
-    .blink {
-        animation: blinker .3s linear 2;
-    }
-    @keyframes blinker {  
-        50% { opacity: 0; }
     }
 </style>
